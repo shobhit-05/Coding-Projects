f2038d1d2765dae034ac6bb17e4ce38329686bfd
Shobhit Gupta <connectshobhitgupta@gmail.com>
2025-03-25 13:56:52 -0400

    Update BMI_Index_calculator.java

--- Java Projects/BMI_Index_Calculator/BMI_Index_calculator.java
+++ Java Projects/BMI_Index_Calculator/BMI_Index_calculator.java
@@ -1,13 +1,11 @@
 /*
 @author: Shobhit Gupta
-@date: April 30 2022
 @description: A BMI Index Calculator
 */
 
+import java.util.*;
 
-import java.util.*; //imports all utility classes, no need for multiple import statements
-
-class Main { //initializes class
+class Main {
 
     static double bmiCalc(double height, double weight) {
         //calculates BMI in function bmiCalc
@@ -28,7 +26,7 @@ class Main { //initializes class
     }
     public static void main(String[] args) {
         //variable declaration and initialization 
-        Scanner uI = new Scanner(System.in); //creation of scanner, which gets user input
+        Scanner input = new Scanner(System.in); //creation of scanner, which gets user input
         List < Double > numberStorage = new ArrayList < Double > (); //creation of list
         //these two are user input variables that take in user Weight and Height
         double userWeight;
@@ -36,23 +34,22 @@ class Main { //initializes class
 
         //captures user input
         System.out.println("Please enter your height in meters: ");
-        userHeight = uI.nextDouble(); //input stored in userHeight
+        userHeight = input.nextDouble(); //input stored in userHeight
         numberStorage.add(userHeight); //input is then added to the list
         double Height = numberStorage.get(0); //height is directly accessible from list
 
         System.out.println("Please enter your weight in kilos: ");
-        userWeight = uI.nextDouble(); //input stored in userWeight
+        userWeight = input.nextDouble(); //input stored in userWeight
         numberStorage.add(userWeight); //input added to list
         double Weight = numberStorage.get(1); //weight then accessible from list
 
-        double BMI = bmiCalc(Height, Weight); //function is called and BMI is calculated with the user input values stored in the list
+        double BMI = bmiCalc(Height, Weight); //BMI is calculated with the user input values stored in the list
         System.out.printf("\nYour BMI is %.2f kg/m2\n", BMI);
         numberStorage.add(BMI); //BMI added into list
-        //System.out.println(numberStorage);
         BMI = numberStorage.get(2); //BMI is now accessible from the list
 
         calculateBMI(BMI);
 
         System.out.println(numberStorage); //provides values inside list
-    } //end of main
-} //end of class
+    }
+}
